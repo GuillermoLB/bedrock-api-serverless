75ce0aed3d7a2f530120ccd8058da747a890c948
GuillermoLB <guillelb99@gmail.com>
2025-02-07 13:20:21 +0100

    Add PostgreSQL configuration and Docker setup: define database settings in config, update docker-compose for database service

--- app/core/config.py
+++ app/core/config.py
@@ -10,6 +10,16 @@ class Settings(BaseSettings):
     LOG_LEVEL: str = "INFO"
     DISABLE_LOGGERS: bool = False
 
+    # PostgreSQL
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = ""
+    POSTGRES_SERVER: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_DB: str = "postgres"
+
+    def get_connection_str(self):
+        return f"postgresql+psycopg2://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
 
 class Config:
     env_file = ".env"
